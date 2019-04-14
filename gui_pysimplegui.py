b20d8c0b76c0e484ab291ba7053049c1da4bca89
sansona <jiaming.justin.chen@gmail.com>
2019-04-14 07:48:09 -0700

    Finished update window on pysimplegui version

--- gui_pysimplegui.py
+++ gui_pysimplegui.py
@@ -2,10 +2,14 @@ import PySimpleGUI as sg
 
 from calculations import recommend_start_amt, recommend_withdrawal
 from monte_carlo import mc_pipeline
+
+# -------------------------------SETUP-----------------------------------------
+
 SIZE = (30, 1)
 
 layout = [
-    [sg.Text('Portfolio information')],
+    [sg.Text('Portfolio information: '), sg.Text(
+        '', size=(70, 1), key='_OUTPUT_')],
     [sg.Text('Starting amount', size=SIZE), sg.InputText()],
     [sg.Text('Annual withdrawal amount', size=SIZE), sg.InputText()],
     [sg.Text('Number years in retirement', size=SIZE), sg.InputText()],
@@ -16,20 +20,30 @@ layout = [
      sg.Button('Monte Carlo'), sg.Button('Exit')]
 ]
 
-window = sg.Window('Simple data entry window').Layout(layout)
+window = sg.Window('Retirement monte carlo').Layout(layout)
+
+# -----------------------------MAIN LOOP---------------------------------------
 
 while True:
-    event, values = window.Read()
-    values = [float(x) for x in values]
-    values[2] = int(values[2])  # n_yrs as int
-    values[5] = int(values[5])  # n_iter as int
+    event, values_dict = window.Read()
+    values = list(values_dict.values())
+    try:
+        values = [float(x) for x in values]
+        values[2] = int(values[2])  # n_yrs as int
+        values[5] = int(values[5])  # n_iter as int
+    except TypeError:
+        # if missing a value, though usually fine since most calculations
+        # dont require all values
+        pass
 
     if event == 'Starting amount':
         start = recommend_start_amt(values[1], values[2], values[4])
-        print('Recommended starting amount: %s' % start)
+        window.FindElement('_OUTPUT_').Update(
+            'Recommended starting amount: %s' % start)
     if event == 'Annual withdrawal':
         low, high = recommend_withdrawal(values[0])
-        print('Recommended annual withdrawal range: %s - %s' % (low, high))
+        window.FindElement('_OUTPUT_').Update(
+            'Recommended annual withdrawal range: %s - %s' % (low, high))
     if event == 'Monte Carlo':
         mc_pipeline(values[0], values[1], values[2], values[3], values[5])
     if event == 'Exit':
